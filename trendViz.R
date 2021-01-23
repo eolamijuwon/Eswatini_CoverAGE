cb0d02529b296d9911305c59ff93f5986373c05c
Emmanuel Olamijuwon <33476964+eolamijuwon@users.noreply.github.com>
2021-01-23 18:44:24 +0200

    updates to codes

--- trendViz.R
+++ trendViz.R
@@ -12,26 +12,31 @@ library(httr)
 url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv" 
 deaths <- read.csv(url)
 
+url_case <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv" 
+cases <- read.csv(url_case)
+
 
 showtext_auto()
 ## Caption Text
-if ("RobotoCondensed-Italic.ttf" %in% list.files("C:\\Windows\\Fonts")) {
+
+if ("AlegreyaSans-MediumItalic.ttf" %in% list.files("C:\\Windows\\Fonts")) {
   
-  font_add("RobotoCondensed-Italic", "RobotoCondensed-Italic.ttf")
-  caption_text <- "RobotoCondensed-Italic"
+  font_add("AlegreyaSans-MediumItalic", "AlegreyaSans-MediumItalic.ttf")
+  text_caption <- "AlegreyaSans-MediumItalic"
   
 } else {
   
   font_add("ARIALNI", "ARIALNI.ttf")
-  caption_text <- "ARIALNI"
+  text_caption <- "ARIALNI"
   
 }
 
+
 ## Graphics Title
-if ("Montserrat-Regular.ttf" %in% list.files("C:\\Windows\\Fonts")) {
+if ("AlegreyaSans-Regular.ttf" %in% list.files("C:\\Windows\\Fonts")) {
   
-  font_add("Montserrat", "Montserrat-Regular.ttf")
-  title_text <- "Montserrat"
+  font_add("AlegreyaSans-Regular", "AlegreyaSans-Regular.ttf")
+  title_text <- "AlegreyaSans-Regular"
   
 } else {
   
@@ -42,14 +47,16 @@ if ("Montserrat-Regular.ttf" %in% list.files("C:\\Windows\\Fonts")) {
 
 
 
+case_counts <- cases %>% select(Country.Region, X1.22.21) %>% filter (Country.Region == "Eswatini")
+total_case <- formatC(case_counts[1,2], big.mark=",")
 
 
 death_counts <- deaths %>% 
-                select(Country.Region, X1.22.20:X1.20.21) %>%
+                select(Country.Region, X1.22.20:X1.22.21) %>%
                 ## Reshape the data from wide format to long
                 ## You can read more about pivot_longer or pivot_wider via the link below:
                 ## https://tidyr.tidyverse.org/reference/#section-pivoting
-                pivot_longer(cols = X1.22.20:X1.20.21, 
+                pivot_longer(cols = X1.22.20:X1.22.21, 
                              names_to = "Date", 
                              values_to = "total_deaths") %>%
                 filter (Country.Region == "Eswatini") %>% 
@@ -79,22 +86,23 @@ new_deaths <- death_counts$new_deaths[13]
 eswatini_logo <- load.image("https://raw.githubusercontent.com/eolamijuwon/Eswatini_CoverAGE/main/Images/Eswatini_flag.png")
 
 death_counts %>%  ggplot() +
-                  geom_step(aes(y = new_deaths,
-                                x = month_year),
-                            group = 1, 
-                            color = "#0B608A",
-                            size = 1) +
                   geom_step (aes(y = total_deaths,
                                  x = month_year),
                              group = 1, 
                              color = "#806C1C",
                              size = 1) + 
+  
+                  geom_step(aes(y = new_deaths,
+                                x = month_year),
+                            group = 1, 
+                            color = "#0B608A",
+                            size = 1) +
                   geom_text (aes (x = month_year,
                                   y = pos,
                                   label = label,
-                                  hjust = 1), size = 10) +
+                                  hjust = 1), size = 11.5) +
                   theme_bw(base_family = title_text,
-                           base_size = 30) + 
+                           base_size = 35) + 
                   theme (                      # Panel
                     panel.grid = element_line(colour = NULL),
                     panel.grid.major.y = element_line(colour = "#D2D2D2",
@@ -106,22 +114,31 @@ death_counts %>%  ggplot() +
                     panel.grid.minor = element_blank()
                   ) +
                   annotate("text", y = 150, angle = 90,
-                           x = 13.2, color = "#0B608A", 
-                           size = 11, label = paste0("New deaths = ", new_deaths)) +
+                           x = 13.2, color = "#0B608A", family = title_text,
+                           size = 15, label = paste0("New deaths = ", new_deaths)) +
                   annotate("text", y = 370, angle = 90,
-                           x = 12.8, color = "#806C1C", 
-                           size = 12, label = paste0("Total deaths = ", total_deaths)) +
+                           x = 12.8, color = "#806C1C", family = title_text, 
+                           size = 15, label = paste0("Total deaths = ", total_deaths)) +
                   annotation_custom(rasterGrob(eswatini_logo),
-                                    ymin = 270, ymax = 350,
+                                    ymin = 340, ymax = 420,
                                     xmin = 1, xmax = 3) +
-                  annotate("text", y = 310, hjust = 0, vjust = 0.5, family = caption_text, 
+                  annotate("text", y = 400, hjust = 0, vjust = 0.5, family = text_caption, 
                            x = 3.2, color = "#000000", lineheight = unit(0.2, "pt"),
-                           size = 18, label = "A total of 222 new deaths from COVID-19 has been recorded in Eswatini 
-                                            \nin the first 20 days of 2021. That's about 17 more deaths than the total 
-                                            \nnumber of deaths from COVID19 complications in 2020 [Mar - Dec].") +
-                  labs (y = "Counts of Deaths", x = "Month and Year")
+                           size = 19, label = paste0("A total of ", new_deaths, " new deaths from COVID-19 has been recorded in Eswatini 
+                                                     \nin the first 22 days of 2021. That is ", (new_deaths - (total_deaths-new_deaths)), " more deaths than the total  
+                                                     \nreported deaths from COVID19 complications in 2020 [Mar - Dec].")) +
+                  annotate("text", y = 325, x = 3.2, hjust = 0, vjust = 0.5, 
+                           family = text_caption, color = "#0D5AA3", lineheight = unit(0.3, "pt"),
+                           size = 20, label = paste0("Cummulative No. of Confirmed Cases: ", total_case)) +
+                  annotate("text", y = 60, x = 1, hjust = 0, vjust = 0.5, 
+                           family = text_caption, color = "#000000", lineheight = unit(0.2, "pt"),
+                           size = 13, label = paste0("Data: Eswatini Government - @EswatiniGovern1 (Twitter)
+                                                     \nVisualization: @eOlamijuwon (Twitter)")) +
+  
+  labs (y = "Counts of Deaths", x = "Month and Year")
   
 
 
 
 ggsave(file="images/Death Counts - SZ.png", dpi=350, height= 6, width= 10)
+
